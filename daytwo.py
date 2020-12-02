66579bb0e68d5018708f0719c5f1f5e1bf34b6a7
fabianhaef <fabian.haefi@gmail.com>
2020-12-02 20:07:29 +0100

    'task2b'

--- daytwo.py
+++ daytwo.py
@@ -21,7 +21,29 @@ for line in f:
     if count >= under_bound and count <= upper_bound:
         correct_password_counter = correct_password_counter + 1
 
-print(correct_password_counter)
+print("Task 1:", correct_password_counter)
+
+
+correct_password_counter = 0
+f = open("daytwo.txt", "r")
+for line in f:
+    line = line.split(sep=' ')
+
+    nums = line[0].split(sep='-')
+    under_bound, upper_bound = int(nums[0].strip()), int(nums[1].strip())
+    char = line[1].strip()
+    char = re.sub(r'[^\w]', ' ', char).strip()
+
+    password = line[2]
+    list(password)
+
+    if (password[under_bound-1] == char and password[upper_bound-1] != char) or (password[under_bound-1] != char and password[upper_bound-1] == char):
+        correct_password_counter = correct_password_counter + 1
+
+
+
+print("Task 2", correct_password_counter)
+
 
 
 
