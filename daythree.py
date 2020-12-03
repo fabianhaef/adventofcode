d9828bb7425c090b41e79094044cc26aaeb3151a
fabianhaef <fabian.haefi@gmail.com>
2020-12-03 21:15:46 +0100

    sol3 co

--- daythree.py
+++ daythree.py
@@ -14,25 +14,23 @@ def solution_1():
 trees_one = solution_1()
 print(trees_one)
 
-def solution_2():
-	slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-
-	def find_trees(right, down):
-		trees = 0
-		size_row = len(f[0]) - 1
-
-		for i in range(0, len(f), down):
-			location = f[i][i*right%size_row]
-			if location == "#":
-				trees += 1
-		return trees
-
-	solution = 1
-	for slope in slopes:
-		solution *= find_trees(slope[0], slope[1])
-
-	return solution
-
-
-trees_two = solution_2()
-print(trees_two)
+with open("daythree.txt") as input:
+    lines = input.readlines()
+    counts = []
+    for right, down in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        x = y = 0
+        count = 0
+        while y <= len(lines) - 1:
+            loc = lines[y][x]
+            if loc == "#":
+                count += 1
+            x = x + right
+            if x >= len(lines[0]) - 1:
+                x = x - len(lines[0]) + 1
+            y = y + down
+        counts.append(count)
+    print(counts)
+    ans = 1
+    for count in counts:
+        ans *= count
+    print(ans)
